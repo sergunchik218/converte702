d029417a1f3b79f909b289078f35b579f43e0674
ThorZol <58010547+sergunchik218@users.noreply.github.com>
2023-08-16 10:17:22 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace _1234
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lable1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.inn = new System.Windows.Forms.TextBox();
+            this.kpp = new System.Windows.Forms.TextBox();
+            this.saveFileDialog2 = new System.Windows.Forms.SaveFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(50, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Вставьте отчет";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lable1
+            // 
+            this.lable1.AutoSize = true;
+            this.lable1.Location = new System.Drawing.Point(203, 77);
+            this.lable1.Name = "lable1";
+            this.lable1.Size = new System.Drawing.Size(0, 13);
+            this.lable1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(50, 263);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Вызовите джина";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // inn
+            // 
+            this.inn.Location = new System.Drawing.Point(50, 120);
+            this.inn.Name = "inn";
+            this.inn.Size = new System.Drawing.Size(100, 20);
+            this.inn.TabIndex = 3;
+            this.inn.Text = "Inn";
+            // 
+            // kpp
+            // 
+            this.kpp.Location = new System.Drawing.Point(50, 159);
+            this.kpp.Name = "kpp";
+            this.kpp.Size = new System.Drawing.Size(100, 20);
+            this.kpp.TabIndex = 4;
+            this.kpp.Text = "kpp";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(916, 559);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 558);
+            this.Controls.Add(this.kpp);
+            this.Controls.Add(this.inn);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lable1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lable1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox inn;
+        private System.Windows.Forms.TextBox kpp;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace _1234
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        XDocument text1;
+        XDocument text2;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "XML-файл |*.xml|All files|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                lable1.Text = openFileDialog1.FileName;
+                text1 = XDocument.Load(lable1.Text);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            text2 = text1;
+            var root = text2.Element("documents");
+            var doc313 = text2.Descendants()?.Elements("register_product_emission");
+            if (doc313.Count() > 0)
+            {
+                var po = new XElement("posting");
+                po.Add(new XAttribute("action_id", "702"));
+                foreach (var node in doc313.Elements())
+                {
+                    po.Add(node);
+                    switch (node.Name.ToString())
+                    {
+                        case "subject_id":
+                           var xe = new XElement("shipper_info");
+                            var inn1 = new XElement("inn");
+                            var kpp2 = new XElement("kpp");
+                            inn1.Value = inn.Text;
+                            kpp2.Value = kpp.Text;
+                            xe.Add(inn1);
+                            xe.Add(kpp2);
+                            po.Add(xe);
+                            break;
+                        case "release_info":
+                            xe = new XElement("doc_num");
+                            xe.Value = node.Element("confirmation_num").Value;
+                            po.Add(xe);
+                            xe = new XElement("doc_date");
+                            xe.Value = node.Element("doc_date").Value;
+                            po.Add(xe);
+                            xe = new XElement("contract_type");
+                            xe.Value = "8";
+                            po.Add(xe);
+                            xe = new XElement("source");
+                            xe.Value = "1";
+                            po.Add(xe);
+                            po.Element("release_info").Remove();
+                            break;
+                        case "signs":
+                            xe = new XElement("order_details");
+                            foreach (var i1 in node.Elements("sscc"))
+                            {
+                               var o1 = new XElement("union");
+                                o1.Add(new XElement("sscc_detail"));
+                                o1.Element("sscc_detail").Add(new XElement(i1));
+                                o1.Add(new XElement("cost"));
+                                o1.Element("cost").Value = "0";
+                                o1.Add(new XElement("vat_value"));
+                                o1.Element("vat_value").Value = "0";
+                                xe.Add(o1);
+                            }
+                            po.Add(xe);
+                            po.Element("signs").Remove();
+                            break;
+                    }      
+                }
+                root.Add(po);
+                doc313.Remove();
+            }
+            var doc431 = text2.Descendants()?.Elements("move_place");
+            if (doc431.Count() >0)
+            {
+                var po = new XElement("posting");
+                po.Add(new XAttribute("action_id", "702"));
+                foreach (var node in doc431.Elements())
+                {
+                    po.Add(node);
+                    switch (node.Name.ToString())
+                    {
+                        case "subject_id":
+                            var xe = new XElement("shipper_info");
+                            var inn1 = new XElement("inn");
+                            var kpp2 = new XElement("kpp");
+                            inn1.Value = inn.Text;
+                            kpp2.Value = kpp.Text;
+                            xe.Add(inn1);
+                            xe.Add(kpp2);
+                            po.Add(xe);
+                            break;
+                        case "receiver_id":
+                            po.Element("receiver_id").Remove();
+                            break;
+                        case "order_details":
+                            xe = new XElement("contract_type");
+                            xe.Value = "8";
+                            po.Add(xe);
+                            xe = new XElement("source");
+                            xe.Value = "1";
+                            po.Add(xe);
+                            xe = new XElement("order_details");
+                            foreach (var i1 in node.Elements("sscc"))
+                            {
+                                var o1 = new XElement("union");
+                                o1.Add(new XElement("sscc_detail"));
+                                o1.Element("sscc_detail").Add(new XElement(i1));
+                                o1.Add(new XElement("cost"));
+                                o1.Element("cost").Value = "0";
+                                o1.Add(new XElement("vat_value"));
+                                o1.Element("vat_value").Value = "0";
+                                xe.Add(o1);
+                              
+                            }
+                            po.Element("order_details").Remove();
+                            po.Add(xe);
+                            break;
+                    }
+                }
+                root.Add(po);
+                doc431.Remove();
+            }
+            var doc415 = text2.Descendants()?.Elements("move_order");
+            if (doc415.Count() > 0)
+            {
+                var po = new XElement("posting");
+                po.Add(new XAttribute("action_id", "702"));
+                foreach (var node in doc415.Elements())
+                {
+                    po.Add(node);
+                    switch (node.Name.ToString())
+                    {
+                        case "subject_id":
+                           var xe = new XElement("shipper_info");
+                            var inn1 = new XElement("inn");
+                            var kpp2 = new XElement("kpp");
+                            inn1.Value = inn.Text;
+                            kpp2.Value = kpp.Text;
+                            xe.Add(inn1);
+                            xe.Add(kpp2);
+                            po.Add(xe);
+                            break;
+                        case "receiver_id":
+                            po.Element("receiver_id").Remove();
+                            break;
+                        case "turnover_type":
+                            po.Element("turnover_type").Remove();
+                            break;
+                        case "source":
+                            xe = new XElement("contract_type");
+                            xe.Value = "1";
+                            po.Add(xe);
+                            po.Element("source").Remove();
+                            break;
+                        case "contract_type":
+                            xe = new XElement("source");
+                            xe.Value = "1";
+                            po.Add(xe);
+                            po.Element("contract_type").Remove();
+                            break;
+                    }               
+
+                }
+                root.Add(po);
+                doc415.Remove();
+            }
+            var doc416 = text2.Descendants()?.Elements("receive_order");
+            if (doc416.Count() > 0)
+            {
+                var po = new XElement("posting");
+                po.Add(new XAttribute("action_id", "702"));
+                foreach (var node in doc416.Elements())
+                {
+                    po.Add(node);
+                    switch (node.Name.ToString())
+                    {
+                        case "subject_id":
+                            var xe = new XElement("shipper_info");
+                            var inn1 = new XElement("inn");
+                            var kpp2 = new XElement("kpp");
+                            inn1.Value = inn.Text;
+                            kpp2.Value = kpp.Text;
+                            xe.Add(inn1);
+                            xe.Add(kpp2);
+                            po.Add(xe);
+                            break;
+                        case "receive_type":
+                            po.Element("receive_type").Remove();
+                            break;
+                        case "source":
+                            xe = new XElement("contract_type");
+                            xe.Value = "1";
+                            po.Add(xe);
+                            po.Element("source").Remove();
+                            break;
+                        case "contract_type":
+                            xe = new XElement("source");
+                            xe.Value = "1";
+                            po.Add(xe);
+                            po.Element("contract_type").Remove();
+                            break;
+                    }
+                }
+                root.Add(po);
+                doc416.Remove();
+            }
+            saveFileDialog1.Filter = "XML-файл |*.xml|All files|*.*";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                text2.Save(saveFileDialog1.FileName);
+            }
+        }
+    }
+}
